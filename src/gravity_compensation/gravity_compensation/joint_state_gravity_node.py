e240b30c1c60c2d44819ecd7ee29f082622b93bf
Sky <eiwofjd3@email.com>
2026-07-31 10:32:50 +0800

    Add configurable robot model parameters

--- src/gravity_compensation/gravity_compensation/joint_state_gravity_node.py
+++ src/gravity_compensation/gravity_compensation/joint_state_gravity_node.py
@@ -9,7 +9,12 @@ class JointStateGravityNode(Node):
 
     def __init__(self):
         super().__init__("joint_state_gravity_node")
-
+        self.declare_parameter("m1", 1.0)
+        self.declare_parameter("m2", 1.0)
+        self.declare_parameter("l1", 1.0)
+        self.declare_parameter("lc1", 0.5)
+        self.declare_parameter("lc2", 0.5)
+        self.declare_parameter("g", 9.81)
         self.subscription = self.create_subscription(
             JointState,
             "/joint_states",
@@ -49,7 +54,23 @@ class JointStateGravityNode(Node):
         q1 = msg.position[joint1_index]
         q2 = msg.position[joint2_index]
 
-        tau1, tau2 = calculate_gravity_torques(q1, q2)
+        m1 = self.get_parameter("m1").value
+        m2 = self.get_parameter("m2").value
+        l1 = self.get_parameter("l1").value
+        lc1 = self.get_parameter("lc1").value
+        lc2 = self.get_parameter("lc2").value
+        g = self.get_parameter("g").value
+
+        tau1, tau2 = calculate_gravity_torques(
+            q1,
+            q2,
+            m1=m1,
+            m2=m2,
+            l1=l1,
+            lc1=lc1,
+            lc2=lc2,
+            g=g,
+        )
         torque_message = Float64MultiArray()
         torque_message.data = [tau1, tau2]
         self.torque_publisher.publish(torque_message)
